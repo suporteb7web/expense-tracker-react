87bf4690b6f21b7c097e9e96a648cfb2d2469d3c
Braian Viacava de Ávila <77290279+bBraian@users.noreply.github.com>
2021-10-31 23:58:32 -0300

    Linha não utilizada causando erro no deploy automático

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
-import { Category } from './types/Category';
 import { categories } from './data/categories';
 import { items } from './data/items';
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
